7cd0a2921adb0f4bfb0a443ec04e4c5f849dc200
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-11 12:45:07 +0630

    Update AuthLayout.jsx

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -5,7 +5,7 @@ const AuthLayout = ({children}) => {
   return (
     <div className='flex'>
         <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+            <h2 className='text-lg font-medium text-black'>Budget<span className='text-yellow-500'>Bee</span></h2>
             {children}
         </div>
         <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
