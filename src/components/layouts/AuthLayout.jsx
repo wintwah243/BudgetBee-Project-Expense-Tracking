44e707f0dcabfb73853a5cb0057ce200bc28bab9
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-13 19:58:54 +0630

    Update AuthLayout.jsx

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import loginSignupimg from "../../assets/images/loginSignup.jpg"
+import { Link } from 'react-router-dom'
 
 const AuthLayout = ({children}) => {
   return (
     <div className='flex'>
         <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <h2 className='text-lg font-medium text-black'>Budget<span className='text-yellow-500'>Bee</span></h2>
+            <Link to="/login"><h2 className='flex text-lg font-medium text-black'>Budget<span className='text-yellow-500'>Bee</span></h2></Link>
             {children}
         </div>
         <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
