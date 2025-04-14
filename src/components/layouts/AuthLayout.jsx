04cc5f03bc31c9ddbf1c6734e320982653074edd
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-14 17:43:50 +0630

    Update AuthLayout.jsx

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import loginSignupimg from "../../assets/images/loginSignup.jpg"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import loginSignupimg from "../../assets/images/loginSignup.jpg";
+import budgetbee from "../../assets/images/BudgetBee.png";
+import { Link } from 'react-router-dom';
 
-const AuthLayout = ({children}) => {
+const AuthLayout = ({ children }) => {
   return (
-    <div className='flex'>
-        <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-            <Link to="/login"><h2 className='flex text-lg font-medium text-black'>Budget<span className='text-yellow-500'>Bee</span></h2></Link>
-            {children}
-        </div>
-        <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
-            
-            <img src={loginSignupimg} className='' />
-        </div>
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <Link to="/login">
+          <div className="flex items-center gap-0">
+            <h2 className="text-lg font-medium text-black">
+              Budget<span className="text-yellow-500">Bee</span>
+            </h2>
+            <img src={budgetbee} alt="BudgetBee Logo" className="w-[70px]" />
+          </div>
+        </Link>
+        {children}
+      </div>
+      
+      <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+        <img src={loginSignupimg} alt="Login Signup Visual" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
